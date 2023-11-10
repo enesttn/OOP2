28dccee1401a922b27e10aafc0d8d58752e3df53
enesttn <83828133+enesttn@users.noreply.github.com>
2023-11-10 12:19:34 +0300

    FileLoggerService.cs

--- OOP3/FileLoggerService.cs
+++ OOP3/FileLoggerService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace OOP3
 {
-    internal class FileLoggerService
+    class FileLoggerService : ILoggerService
     {
+        public void Log()
+        {
+            Console.WriteLine("Dosyaya Loglandı.");
+        }
     }
 }
