86e5155e0777cabd075ec68dccc2ca3f11043d1d
enesttn <83828133+enesttn@users.noreply.github.com>
2023-11-10 12:17:44 +0300

    BasvuruManager.cs

--- OOP3/BasvuruManager.cs
+++ OOP3/BasvuruManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace OOP3
 {
-    internal class BasvuruManager
-    {
-    }
+     class BasvuruManager
+	{
+		//Method Injection
+		public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+		{
+			// Başvuran bilgilerini değerlendirme
+			
+			krediManager.Hesapla();
+			loggerService.Log();
+		}
+
+		public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+		{
+			foreach (var kredi in krediler)
+			{
+				kredi.Hesapla();
+			}
+		}
+
+	}
 }
